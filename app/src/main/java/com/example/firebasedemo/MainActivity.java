612c14d1996312c64946ae2dbcc6d44c550f44dc
srijalkc <srijalkc2017@gmail.com>
2019-05-01 01:15:41 +0545

    Code Implemented with 1 dependency on app build.gradle

--- app/src/main/java/com/example/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebasedemo/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.firebasedemo;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button buttonRegister;
+    private TextView textViewSignIn;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressDialog = new ProgressDialog(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        textViewSignIn = (TextView) findViewById(R.id.textViewSignIn);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonRegister.setOnClickListener(this);
+
+        textViewSignIn.setOnClickListener(this);
+
+        editTextEmail.setOnClickListener(this);
+        editTextPassword.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)){
+            //email is emptty
+            Toast.makeText(this,"Please Enter Email", Toast.LENGTH_SHORT).show();
+            //stopping the further execution of code
+            return;
+            }
+
+        if(TextUtils.isEmpty(password)){
+            //password is empty
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_LONG).show();
+            //stopping the further execution of code
+            return;
+            }
+        //if validations are ok
+        //we will display progressbar as it will take time to register because we are usint internet
+
+        progressDialog.setMessage("Registering User...");
+        progressDialog.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            //user is successfully registered and logged in
+                            //we will start the profile activity here
+                            //right now lets display toast only
+                            Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                            }
+                        else{
+                            Toast.makeText(MainActivity.this, "Registered Unsuccessful", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            registerUser();
+        }
+        if(v == textViewSignIn){
+            //will open sign-in activity
+        }
+
     }
 }
